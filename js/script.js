2ff08d02fbf99058dac142970f33d4227bf63f67
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-12 23:43:49 -0800

    Completed Slice of Pie Part II

--- js/script.js
+++ js/script.js
@@ -12,15 +12,21 @@ console.log("Spark is " + humanY + " " + "human years old which is " + dogY + " 
 //PROBLEM 2: Calculate how much pizza each partygoer will get at the party.
 
 //# of slices each pizza has
-var slices = 16;
+var slices = 12;
 //# of people at the party
-var people = 50;
+var people = 20;
 //# of pizzas ordered
-var pizzas = 12;
+var pizzas = 4;
 
 //Result of how many slices of pizza each person will receive
 var portion = slices * pizzas / people;
 
 //Print Result
 console.log("Each person ate " + portion + " " + "slices of pizza at the party.");
+
+//PROBLEM 3: Calculate how many left over slices Sparky will eat
+
+var sparkySlices = slices * pizzas % people;
+
+console.log("Sparky got " + sparkySlices + " " + "slices of pizza.");
 
