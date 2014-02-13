30e0692b73c16311a3c38823ca9eea09f80b6408
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-12 23:25:44 -0800

    Problem 2 variable set up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Calculate how old Sparky the dog is in dog years compared to human years.
+//PROBLEM 1: Calculate how old Sparky the dog is in dog years compared to human years.
 
 //Sparky's age in human years
 var humanY = 3;
@@ -7,3 +7,13 @@ var dogY = humanY * 7;
 
 //Print out Sparky's age in dog years
 console.log("Spark is " + humanY + " " + "human years old which is " + dogY + " " + "in dog years.");
+
+
+//PROBLEM 2: Calculate how much pizza each partygoer will get at the party.
+
+//# of slices each pizza has
+var slices = 16;
+//# of peopel at the party
+var people = 50;
+//# of pizzas ordered
+var pizzas = 12;
