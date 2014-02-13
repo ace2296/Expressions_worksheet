3e2db990b143bec30439d0643cba049784ce605e
Andre Guerra <AndreGuerra@Andres-MacBook-Pro.local>
2014-02-12 23:18:03 -0800

    Print out Sparky's age in dog and human years

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,6 @@
 var humanY = 3;
 //Sparky's age in dog years
 var dogY = humanY * 7;
+
+//Print out Sparky's age in dog years
+console.log("Spark is " + humanY + " " + "human years old which is " + dogY + " " + "in dog years.");
